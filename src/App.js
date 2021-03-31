fd8bd32a7c4d4f3f40e3448430a682d3e964173c
Alireza <ar.namayandeh@gmail.com>
2021-03-31 10:36:02 -0400

    NewsCard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+import NewsCards from './components/NewsCards/NewsCards'; 
+import NewCards from './components/NewsCards/NewsCards';
+
 // API key: 312800ce07b1435dba88d194ea557102
 const alanKey = 'c8fb6fbd5464157973623c8c05f76ee22e956eca572e1d8b807a3e2338fdd0dc/stage';
 
@@ -21,6 +24,7 @@ const App = () =>{
     return(
         <div>
             <h1>Alan AI News</h1>
+            <NewCards articles={newsArticles} />
         </div>
     );
 }
---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NewsCard = ({articles}) => {
+    return(
+        <div>
+            News Card 
+        </div>
+    );
+}
+
+export default NewsCard;    

c2b5e06f72c6a5bc23af3c31801e19dff90da369
Alireza <ar.namayandeh@gmail.com>
2021-03-31 15:58:16 -0400

    Adding NewsCards to the main page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, {useState, useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards'; 
-import NewCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+
+
 
 // API key: 312800ce07b1435dba88d194ea557102
 const alanKey = 'c8fb6fbd5464157973623c8c05f76ee22e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
 
     useEffect(()=>{
         alanBtn({
@@ -24,8 +27,10 @@ const App = () =>{
 
     return(
         <div>
-            <h1>Alan AI News</h1>
-            <NewCards articles={newsArticles} />
+            <div className={classes.logoContainer}>
+                <img src="https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg" className={classes.alanLogo} alt="Alan Logo" />
+            </div>
+            <NewsCards articles={newsArticles} />
         </div>
     );
 }
